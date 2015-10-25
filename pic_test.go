381133f80e84b49d7b1f24500ca743da72ed6e8d
The Mule <3.7mules@gmail.com>
2015-10-24 23:58:15 -0500

    Don't need sep package for simple image test

--- pic_test.go
+++ pic_test.go
@@ -1,25 +1,24 @@
-package picture
+package hexagon
 
 import (
 	"fmt"
 	"github.com/llgcode/draw2d"
 	"github.com/llgcode/draw2d/draw2dimg"
 	"image"
-	//	"image/color"
+	"image/color"
 	"image/draw"
 	"log"
-	"mule/hexagon"
 	"testing"
 )
 
-func TestFirst(t *testing.T) {
+func TestFirstPIC(t *testing.T) {
 	log.Println("TESTING")
 }
 
-func TestSecond(t *testing.T) {
+func TestSecondPIC(t *testing.T) {
 	draw2d.SetFontFolder("")
 	dest := image.NewRGBA(image.Rect(0, 0, 400, 400))
-	white := Pallate("white")
+	white := color.RGBA{0xff, 0xff, 0xff, 255}
 	draw.Draw(dest, dest.Bounds(), &image.Uniform{white}, image.ZP, draw.Src)
 	gc := draw2dimg.NewGraphicContext(dest)
 	gc.SetFontData(draw2d.FontData{Name: "DroidSansMono", Family: draw2d.FontFamilyMono})
@@ -28,25 +27,24 @@ func TestSecond(t *testing.T) {
 	v := GetVP()
 	for x := 0; x < 400; x++ {
 		for y := 0; y < 400; y++ {
-			p := hexagon.Pixel{float64(x), float64(y)}
+			p := Pixel{float64(x), float64(y)}
 			hex := v.HexContaining(p)
 			//hex := v.HexContaining(p.Flip())
-			clr := Pallate("blue")
+			var clr color.RGBA
 			clN := (hex[0] - hex[1]) % 4
 			for clN < 0 {
 				clN += 4
 			}
 			switch clN {
-			case 0:
-				clr = Pallate("blue")
-			case 1:
-				clr = Pallate("green")
-			case 2:
-				clr = Pallate("red")
-			default:
-				clr = Pallate("white")
+			case 0: // blue
+				clr = color.RGBA{0x00, 0x33, 0xcc, 255}
+			case 1: // green
+				clr = color.RGBA{0x00, 0x66, 0x00, 255}
+			case 2: // red
+				clr = color.RGBA{0x99, 0, 0, 255}
+			default: // white
+				clr = white
 			}
-			_ = clr
 			dest.Set(x, y, clr)
 			/*if (x+60)%60 == 0 && y%15 == 0 {
 				//pt := v.CenterOf(hex)
@@ -59,7 +57,7 @@ func TestSecond(t *testing.T) {
 		for y := -5; y < 5; y++ {
 			//for x := 0; x < 2; x++ {
 			//for y := 0; y < 2; y++ {
-			h := v.CenterHex.Add(hexagon.Coord{x, y})
+			h := v.CenterHex.Add(Coord{x, y})
 			corners := v.CornersOf(h)
 			/*for i, cn := range corners {
 				corners[i] = cn.Flip()
@@ -85,10 +83,10 @@ func TestSecond(t *testing.T) {
 	draw2dimg.SaveToPngFile("picture.png", dest)
 }
 
-func GetVP() *hexagon.Viewport {
-	v := hexagon.MakeViewport(40.0)
-	v.CenterPix = hexagon.Pixel{100, 200}
-	v.CenterHex = hexagon.Coord{10, 10}
+func GetVP() *Viewport {
+	v := MakeViewport(40.0)
+	v.CenterPix = Pixel{100, 200}
+	v.CenterHex = Coord{10, 10}
 	//v.HexH *= .75 // For isometric slant; it's that easy!
 	//v.HexW *= .75 // For flipped isometric slant; it's that easy!
 	return v
